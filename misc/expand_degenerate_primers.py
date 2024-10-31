b26e67a019655a94ed836ae0da94ea8f1219aef3
Chris <Rpinerd@gmail.com>
2024-10-31 11:45:37 -0400

    Flake8/Black formatting and best practices

--- misc/expand_degenerate_primers.py
+++ misc/expand_degenerate_primers.py
@@ -1,25 +1,26 @@
 import sys
 
 _degenerate_base_codes_ = {
-    "R" : ["A","G"],
-    "Y" : ["C","T"],
-    "M" : ["A","C"],
-    "K" : ["G","T"],
-    "S" : ["G","C"],
-    "W" : ["A","T"],
-    "H" : ["A","T","C"],
-    "B" : ["G","T","C"],
-    "D" : ["G","A","T"],
-    "V" : ["G","A","C"],
-    "N" : ["A","C","G","T"]
+    "R": ["A", "G"],
+    "Y": ["C", "T"],
+    "M": ["A", "C"],
+    "K": ["G", "T"],
+    "S": ["G", "C"],
+    "W": ["A", "T"],
+    "H": ["A", "T", "C"],
+    "B": ["G", "T", "C"],
+    "D": ["G", "A", "T"],
+    "V": ["G", "A", "C"],
+    "N": ["A", "C", "G", "T"],
 }
 
+
 def expand_primer(primer):
-    ''' Expand degenerate bases in a primer
+    """Expand degenerate bases in a primer
     :param str primer
     :yields primer(s) with degenerate bases expanded
-    '''
-    assert primer!="", "Require valid primer sequence!"
+    """
+    assert primer != "", "Require valid primer sequence!"
 
     degen_bases = []
     degen_bases_loc = []
@@ -38,26 +39,34 @@ def expand_primer(primer):
 
         for bases in combinations:
             temp = list(primer)
-            for i,base in enumerate(bases):
+            for i, base in enumerate(bases):
                 base_to_modify = degen_bases_loc[i]
-                assert temp[base_to_modify] not in ["A","C","G","T"], "Error in Logic !!"
+                assert temp[base_to_modify] not in ["A", "C", "G", "T"], "Error in Logic !!"
                 temp[base_to_modify] = base
 
             yield "".join(temp)
 
 
 def iterate_primer_file(primer_file):
-    ''' Iterate over the primer file , expand degenerate bases and write to a new file
+    """Iterate over the primer file , expand degenerate bases and write to a new file
     :param str primer_file : A tsv file with information on the primers
-    '''
-    with open(primer_file,"r") as IN, open(primer_file+".expanded","w") as OUT:
+    """
+    with open(primer_file, "r") as IN, open(primer_file + ".expanded", "w") as OUT:
         for line in IN:
             contents = line.strip("\n\r").split("\t")
-            amplicon_id,forward_p_id,forward_p,reverse_p_id,reverse_p = contents
+            amplicon_id, forward_p_id, forward_p, reverse_p_id, reverse_p = contents
 
-            if line.startswith("Region"): # header
-                out = [amplicon_id,forward_p_id,"Forward Primer Name",forward_p,reverse_p_id,"Reverse Primer Name",reverse_p]
-                OUT.write("\t".join(out)+"\n")
+            if line.startswith("Region"):  # header
+                out = [
+                    amplicon_id,
+                    forward_p_id,
+                    "Forward Primer Name",
+                    forward_p,
+                    reverse_p_id,
+                    "Reverse Primer Name",
+                    reverse_p,
+                ]
+                OUT.write("\t".join(out) + "\n")
                 continue
 
             if forward_p != "":
@@ -70,13 +79,22 @@ def iterate_primer_file(primer_file):
             else:
                 reverse_expanded_primers = [""]
 
-            i=1
-            j=1
+            i = 1
+            j = 1
             for pf in forward_expanded_primers:
                 for pr in reverse_expanded_primers:
-                    out = [amplicon_id,forward_p_id,forward_p_id+"_"+str(i),pf,reverse_p_id,reverse_p_id+"_"+str(j),pr]
-                    OUT.write("\t".join(out)+"\n")
-                    j+=1
-                i+=1
+                    out = [
+                        amplicon_id,
+                        forward_p_id,
+                        forward_p_id + "_" + str(i),
+                        pf,
+                        reverse_p_id,
+                        reverse_p_id + "_" + str(j),
+                        pr,
+                    ]
+                    OUT.write("\t".join(out) + "\n")
+                    j += 1
+                i += 1
+
 
 iterate_primer_file(sys.argv[1])
